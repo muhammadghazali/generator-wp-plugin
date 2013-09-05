9c101ca124d3c5255f3d2dc6e4234259a6526ef0
Muhammad Ghazali <muhammadghazali2480@gmail.com>
2013-09-05 15:59:05 +0700

    add more prompts to complete the plugin information header
    The user of this generator should the plugin name, plugin URI, plugin description, plugin version, plugin author, plugin author URI, and plugin license slug such as MIT.

---
+++ app/templates/_main.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: <%= pluginName %>
+Plugin URI: <%= pluginURI %>
+Description: <%= pluginDescription %>
+Version: <%= pluginVersion %>
+Author: <%= pluginAuthor %>
+Author URI: <%= pluginAuthorURI %>
+License: <%= pluginLicense %>
+*/
+?>
